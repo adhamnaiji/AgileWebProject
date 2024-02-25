a12c656aacf9a22adc5551f1a38fcccc34e57741
adham <adhemnaiji@gmail.com>
2024-02-25 18:27:53 +0100

    MainP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../Components/navbar/navbar.component';
+import { MainPageComponent } from '../Components/main-page/main-page.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent],
+  imports: [RouterOutlet,NavbarComponent,MainPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
