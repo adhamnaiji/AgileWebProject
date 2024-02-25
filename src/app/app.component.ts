9e3b3a45f634216c2d32a0bf01741ac581534be7
adham <adhemnaiji@gmail.com>
2024-02-26 00:41:59 +0100

    front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../Components/navbar/navbar.component';
 import { MainPageComponent } from '../Components/main-page/main-page.component';
+import { SignInComponent } from '../Components/sign-in/sign-in.component';
+import { SignUpComponent } from '../Components/sign-up/sign-up.component';
+import { ResetPaswwordComponent } from '../Components/reset-paswword/reset-paswword.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,MainPageComponent],
+  imports: [RouterOutlet,NavbarComponent,MainPageComponent,SignInComponent,SignUpComponent,ResetPaswwordComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
